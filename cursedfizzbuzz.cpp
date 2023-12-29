af821b7d7293e7454529af8ac909d0e951759215
Heyden <112825785+haydenlikehey@users.noreply.github.com>
2023-12-29 15:59:16 -0600

    nums in hex, chars in bin, main in oct

--- cursedfizzbuzz.cpp
+++ cursedfizzbuzz.cpp
@@ -2,17 +2,17 @@
 
 int main (void) {
 
-  int* numbers = new int[100] {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25,26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100};
+  int* numbers = new int[0144] {0x1, 0x2, 0x3, 0x4, 0x5, 0x6, 0x7, 0x8, 0x9, 0xA, 0xB, 0xC, 0xD, 0xE, 0xF, 0x10, 0x11, 0x12, 0x13, 0x14, 0x15, 0x16, 0x17, 0x18, 0x19 , 0x1A, 0x1B, 0x1C, 0x1D, 0x1E, 0x1F, 0x20, 0x21, 0x22, 0x23, 0x24, 0x25, 0x26, 0x27, 0x28, 0x29, 0x2A, 0x2B, 0x2C, 0x2D, 0x2E, 0x2F, 0x30, 0x31, 0x32, 0x33, 0x34, 0x35, 0x36, 0x37, 0x38, 0x39, 0x3A, 0x3B, 0x3C, 0x3D, 0x3E, 0x3F, 0x40, 0x41, 0x42, 0x43, 0x44, 0x45, 0x46, 0x47, 0x48, 0x49, 0x4A, 0x4B, 0x4C, 0x4D, 0x4E, 0x4F, 0x50, 0x51, 0x52, 0x53, 0x54, 0x55, 0x56, 0x57, 0x58, 0x59, 0x5A, 0x5B, 0x5C, 0x5D, 0x5E, 0x5F, 0x60, 0x61, 0x62, 0x63, 0x64};
 
-  char* fizzbuzz = new char[] {'F', 'i', 'z', 'z', 'B', 'u', 'z', 'z'};
+  char* fizzbuzz = new char[] {0b01000110, 0b01101001, 0b01111010, 0b01111010, 0b01000010, 0b01110101, 0b01111010, 0b01111010};
 
-  int integer = 0;
+  int integer = 00;
 
   loop:
 
-    if ((*(numbers) + integer) % 3 != 0 && (*(numbers) + integer) % 5 != 0 && (*(numbers) + integer) % 15 != 0) {
+    if ((*(numbers) + integer) % 03 != 00 && (*(numbers) + integer) % 05 != 00 && (*(numbers) + integer) % 017 != 0) {
       
-      if (integer >= 100) {
+      if (integer >= 0144) {
         goto exit;
       }
       std::cout << (*(numbers) + integer) << std::endl;
@@ -20,56 +20,56 @@ int main (void) {
     } else {
 
       try {
-        if (*((numbers) + integer) % 15 == 0) {
-          throw 15;
+        if (*((numbers) + integer) % 017 == 0) {
+          throw 017;
         }
       }
       catch (int fizzybuzzy) {
-        for (int k = 0; k < 8; k = k + 1) {
+        for (int k = 00; k < 010; k = k + 01) {
           std::cout << *((fizzbuzz) + k);
         }
         std::cout << std::endl;
-        integer = integer + 1;
+        integer = integer + 01;
         goto loop;
       }
 
       try {
-        if (*((numbers) + integer) % 5 == 0) {
+        if (*((numbers) + integer) % 05 == 00) {
           throw 5;
         }
       }
       catch (int buzz) {
-        for (int j = 4; j < 8; j++) {
+        for (int j = 04; j < 010; j++) {
           std::cout << *((fizzbuzz) + j);
         }
         std::cout << std::endl;
-        integer = integer + 1;
+        integer = integer + 01;
         goto loop;
       }
 
       try {
-        if (*((numbers) + integer) % 3 == 0) {
-          throw 3;
+        if (*((numbers) + integer) % 03 == 00) {
+          throw 03;
         }
       }
       catch (int buzz) {
-        for (int i = 0; i < 4; ++i){
+        for (int i = 00; i < 04; ++i){
           std::cout << *((fizzbuzz) + i);
         }
           std::cout << std::endl;
-          integer = integer + 1;
+          integer = integer + 01;
           goto loop;
       }
 
     }
 
-    integer = integer + 1;
+    integer = integer + 01;
   
-  if (integer < 100) {
+  if (integer < 0144) {
     goto loop;
   }
 
   exit:
-  return 1;
+  return 01;
 
 }
